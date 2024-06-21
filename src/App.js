c544ae2a6041000fa6b2c0e11443df76e3878c66
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2024-06-20 22:11:09 -0300

    add: mobile alert screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import { BrowserRouter } from "react-router-dom"
 import "./App.css"
 import { SideMenu } from "./components/menu/SideMenu"
 import Content from "./router/content"
+import useWindowDimensions from "./utils/functions"
+import { DesktopAlert } from "./router/DesktopAlert"
 
 function App() {
+  const { width } = useWindowDimensions()
+  const isDesktop = width > 768
+
   return (
     <BrowserRouter>
-      <SideMenu />
-      <Content />
+      {isDesktop ? (
+        <>
+          <SideMenu />
+          <Content />
+        </>
+      ) : (
+        <DesktopAlert />
+      )}
     </BrowserRouter>
   )
 }
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,9 +1,28 @@
-export function getWindowDimensions() {
+import { useState, useEffect } from "react"
+
+function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window
   return {
     width,
     height,
   }
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  )
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions())
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return windowDimensions
 }
 
 export function onEnterSearch(e) {
