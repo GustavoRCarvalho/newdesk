ce11a78fbcb1d7ca558aae20246b8dde16db379a
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2024-07-17 13:59:49 -0300

    add: first article change background color

--- src/components/principal/editor/Editor.jsx
+++ src/components/principal/editor/Editor.jsx
@@ -4,7 +4,11 @@ import { EditorComponent } from "./EditorComponent"
 import styled from "styled-components"
 import { useEffect, useState } from "react"
 import { CategoryDropdown } from "./CategoryDropdown"
-import { initialData, setEditorInitial } from "../../../store/editorSlice"
+import {
+  initialData,
+  setEditor,
+  setEditorInitial,
+} from "../../../store/editorSlice"
 import { SubCategoryDropdown } from "./SubCategoryDropdown"
 import { ArticleDropdown } from "./ArticleDropdown"
 import { readJsonFile, updateJsonFile } from "../../../utils/googleDriveApi"
@@ -17,11 +21,21 @@ import { createAlertError, createAlertSucess } from "../../../store/alertSlice"
 export const Editor = () => {
   const location = useLocation()
   const dispatch = useDispatch()
+  const [backgroundColor, setBackgroundColor] = useState(0)
+  const colors = [
+    "var(--home-card-background)",
+    "#3f7ee8",
+    "#66be67",
+    "#f3c324",
+    "#ef9441",
+    "#df5e5a",
+  ]
   const [errorMessage, setErrorMessage] = useState("")
   const [isSaving, setIsSaving] = useState(false)
   const [fileFound, setFileFound] = useState(false)
   const editorState = useSelector((state) => state.editor)
   const editorData = editorState.environment
+  let copyContent = JSON.parse(JSON.stringify(editorData.categories))
 
   const dataEditor = () => {
     const content =
@@ -47,6 +61,28 @@ export const Editor = () => {
       setErrorMessage(e.message)
       setFileFound(false)
     }
+  }
+
+  const handleChangeColor = () => {
+    if (
+      editorState.selectedCategoryIndex === -1 ||
+      editorState.selectedSubCategoryIndex === -1 ||
+      editorState.selectedArticleIndex === -1
+    ) {
+      return
+    }
+
+    if (backgroundColor === colors.length - 1) {
+      setBackgroundColor(0)
+      return
+    }
+    setBackgroundColor((state) => (state += 1))
+
+    copyContent[editorState.selectedCategoryIndex].subCategories[
+      editorState.selectedSubCategoryIndex
+    ].articles[editorState.selectedArticleIndex].backgroundColor =
+      colors[backgroundColor]
+    dispatch(setEditor(copyContent))
   }
 
   async function saveData() {
@@ -78,7 +114,10 @@ export const Editor = () => {
   return !fileFound ? (
     <LoadingScreen errorMessage={errorMessage} />
   ) : (
-    <EditorContainer>
+    <EditorContainer
+      $readOnly={!dataEditor()}
+      $backgroundColor={colors[backgroundColor]}
+    >
       <DropdownContainer>
         <CategoryDropdown />
         <SubCategoryDropdown />
@@ -90,6 +129,7 @@ export const Editor = () => {
         <ReactQuill readOnly theme="snow" modules={modules} />
       )}
       <SaveContainer>
+        <button onClick={handleChangeColor}>Trocar cor do fundo</button>
         <button onClick={saveData}>Salvar {isSaving && <Spinner />}</button>
       </SaveContainer>
     </EditorContainer>
@@ -136,8 +176,6 @@ const EditorContainer = styled.div`
   .quill {
     width: 100%;
     height: 100%;
-
-    margin-bottom: 2em;
   }
 
   .ql-toolbar {
@@ -152,19 +190,61 @@ const EditorContainer = styled.div`
     background-color: var(--home-card-background);
   }
 
+  .ql-color-picker.ql-color .ql-picker-item:hover {
+    border-color: var(--editor-color);
+  }
+
+  .ql-toolbar.ql-snow {
+    text-align: center;
+    .ql-picker.ql-expanded {
+      .ql-picker-label {
+        border: 1px solid var(--editor-color);
+        border-radius: 0.5em;
+        color: var(--editor-color-hover);
+      }
+      .ql-picker-options {
+        border-radius: 0.5em;
+        border: none;
+      }
+    }
+
+    button:hover,
+    button:focus,
+    .ql-picker-label:hover,
+    .ql-picker-item:hover,
+    .ql-picker-item.ql-selected {
+      color: var(--editor-color-hover);
+      .ql-stroke {
+        stroke: var(--editor-color-hover);
+      }
+      .ql-fill {
+        fill: var(--editor-color-hover);
+      }
+    }
+  }
+
+  .ql-picker-item {
+  }
+
   .ql-container {
     border: none;
 
     margin-top: 2em;
+    height: calc(100% - (24px + 5em));
   }
 
   .ql-editor {
-    background-color: var(--home-card-background);
-    height: calc(100% - (24px + 2em));
+    background-color: ${(props) =>
+      props.$backgroundColor ?? "var(--home-card-background)"};
+
+    height: 100%;
 
     border-radius: 0.5em;
 
     box-shadow: 0em 0em 1em 0em #0000004b;
+    * {
+      cursor: ${(props) => (props.$readOnly ? "default" : "text")};
+    }
   }
 
   .ql-fill {
@@ -189,7 +269,7 @@ const DropdownContainer = styled.div`
 
 const SaveContainer = styled.div`
   display: flex;
-  justify-content: end;
+  justify-content: space-between;
 
   width: 100%;
   max-width: 920px;
